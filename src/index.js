d80f3317826f12e22f05b2bbd7f918f97e8423b5
Eric Ndung'u <ericobain@gmail.com>
2019-03-31 10:42:19 +0300

    chore setup webpack
    - install project dependencies
    - configure webpack
    - configure babel

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    return (
+        <div>Welcome to Sendit</div>
+    );
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
